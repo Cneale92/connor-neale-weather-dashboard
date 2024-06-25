bdaf9f27389ec6b88a2982db4696fcac04eef6dc
Cneale92 <connorneale92@gmail.com>
2024-06-24 21:57:46 -0500

    added date and time

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,5 @@ document.getElementById(`right-content`).style.visibility = `hidden`;
 
 const apiKey = `c16b89d5a85d543c78bba4d012495c74`;
 
+const now = dayjs();
+let tomorrow = now.add(1, "day").format("DD/MM/YYYY");
