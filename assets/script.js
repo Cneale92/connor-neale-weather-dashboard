a0c0f377b45632ea0bdc3d04046df149a7cab069
Cneale92 <connorneale92@gmail.com>
2024-06-24 21:56:04 -0500

    added apikey

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,10 @@ const cityNameEl = document.querySelector("#cityName");
 const cityTempEl = document.querySelector("#cityTemp");
 const cityWindEl = document.querySelector("#cityWind");
 const cityHumidityEl = document.querySelector("#cityHumidity");
+
+const pastCitiesEl = document.querySelector("#pastCities");
+
+document.getElementById(`right-content`).style.visibility = `hidden`;
+
+const apiKey = `c16b89d5a85d543c78bba4d012495c74`;
+
