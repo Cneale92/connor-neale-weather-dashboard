9cb7c1701e7fd3ca4cb5cc6a78f561857803b0fc
Cneale92 <connorneale92@gmail.com>
2024-06-24 21:53:18 -0500

    added form, input fields and text elements for cities

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+const cityFormEl = document.querySelector("#city-form");
+const cityInputEl = document.querySelector("#cityInput");
+
+const cityNameEl = document.querySelector("#cityName");
+const cityTempEl = document.querySelector("#cityTemp");
+const cityWindEl = document.querySelector("#cityWind");
+const cityHumidityEl = document.querySelector("#cityHumidity");
